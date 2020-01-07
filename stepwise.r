3020f864bb96ac6f4a4b10cfcb1452aa4ac0f669
Yuhao Lin <lyh970817@yandex.com>
2020-01-07 21:52:08 +0000

    Add numeric version for categorical variables

--- stepwise.r
+++ stepwise.r
@@ -4,16 +4,22 @@ load("~/Downloads/stepwise (1).RData")
 # Read googlesheet
 sheet <- stepwise_sheet("Stepwise_variables")
 
-sheet[["variable.swedish"]][sheet[["variable.english"]] == "weight.t1"]
-
-sheet_extract("Min", "vikt1", sheet)
 # Run test
 test_nonames(sheet, stepwise)
 
 # Clean data
+
+# Recode and applying limits
 stepwise_clean <- stepwise_recode_df(stepwise, sheet)
 
-range(stepwise_clean[["weight.t1"]])
+# Which variables are factors?
+which_factor <- which(map_lgl(stepwise_clean, is.factor))
+
+# Create numeric version variable names
+numeric_names <- paste0(names(which_factor), ".numeric")
+
+# Assign numeric version variables
+stepwise_clean[numeric_names] <- map_df(stepwise_clean[which_factor], as.numeric)
 
 # Export data
 save(stepwise_clean, file = "./stepwise_clean.Rdata")
